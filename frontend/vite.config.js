6b1490a1c7dbf482931073323e38c6890feb1345
rancerenly <rancerenly@gmail.com>
2024-10-28 21:50:06 +0300

    настройка проекта, алиасы, форматирование

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,14 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '@/assets/scss/app.scss';`,
+      },
     },
   },
   server: {
