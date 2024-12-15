3c393570d0ad4299a432b6f0158375428f9bf3f6
rancerenly <rancerenly@gmail.com>
2024-12-15 19:37:05 +0300

    неправильное отображение добавок в пиццу

--- frontend/src/common/helpers/get-image.js
+++ frontend/src/common/helpers/get-image.js
@@ -1,6 +1,5 @@
 export const getImage = (path) => {
     const publicUrl = "http://localhost:3000";
     const divider = path.startsWith("/") ? "" : "/";
-    console.log('[publicUrl, path].join(divider)', [publicUrl, path].join(divider));
     return [publicUrl, path].join(divider);
 };
